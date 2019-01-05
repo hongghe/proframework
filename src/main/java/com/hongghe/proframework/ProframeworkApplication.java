8ceb01ef55eaf1fcc24dc8b0a10e302e7e54ccdc
hongghe <364930855@qq.com>
2019-01-05 11:39:09 +0800

    Initial commit

---
+++ src/main/java/com/hongghe/proframework/ProframeworkApplication.java
@@ -0,0 +1,14 @@
+package com.hongghe.proframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProframeworkApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hongghe/proframework/ProframeworkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hongghe.proframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProframeworkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
